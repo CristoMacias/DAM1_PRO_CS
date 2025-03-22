86b39123d69dbb9550b82028b3039f6d36adc1b0
smmonino <smmoninog01@iesalbarregas.es>
2025-03-22 16:45:51 +0100

    Restructuracion 2 y hecha la comprobacion de la existencia del archivo de config.ini, en caso de que no, se crea

---
+++ App.java
@@ -0,0 +1,34 @@
+/**
+ * Videojuego
+ * 
+ * @author Sandra Moñino, Cristo Macias
+ * @version 1.0
+ * @since 2025-03-22
+ * Licencia: GPL v3
+ */
+
+ import java.nio.file.Path;
+ import java.nio.file.Paths;
+ import java.nio.file.Files; 
+ import java.io.IOException;
+public class App{
+	
+	public static void main(String[] args){
+		Path path=Paths.get("");//Ruta donde se encuentra el App
+		Path pathConfiguracion = Paths.get("config.ini"); //Ruta del archivo de config
+		
+		try{
+			if(Files.exists(pathConfiguracion)){ //Comprobar si existe el archivo de config
+				//System.out.println("Ya existe el archivo.");
+			}else{
+				System.out.println("El archivo de configuración no existe y se va a crear.");
+				Files.createFile(pathConfiguracion); //Creamos el archivo config
+			}
+
+		}catch(IOException e){ //Capturamos las exceptiones 
+			e.printStackTrace();
+		}
+		
+	
+	}
+}
