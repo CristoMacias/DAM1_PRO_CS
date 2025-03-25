e80e1a29c2adecb305249a96b431e009cabf8462
Cristo Macias <cmaciasi01@iesalbarregas.es>
2025-03-25 12:24:57 +0100

    Corrección en la creación de directorios

--- App.java
+++ App.java
@@ -11,7 +11,6 @@
  import java.nio.file.Paths;
  import java.nio.file.Files; 
  import java.io.IOException;
- import java.util.Scanner;
 public class App{
 	
 	public static void main(String[] args){
@@ -20,37 +19,22 @@ public class App{
 		
 		try{
 			if(Files.exists(pathConfiguracion)){ //Comprobar si existe el archivo de config
-				//System.out.println("Ya existe el archivo.");
+				System.out.println("Ya existe el archivo.");
 			}else{
 				System.out.println("El archivo de configuración no existe y se va a crear.");
 				Files.createFile(pathConfiguracion); //Creamos el archivo config
+				Path pathEscenas = path.resolve("escenas");
+				Files.createDirectories(pathEscenas);
+				Path pathPersonajes = path.resolve("personajes");
+				Files.createDirectories(pathPersonajes);
+				Path pathPartidas = path.resolve("partidas");
+				Files.createDirectories(pathPartidas);
+				System.out.println("Se han creado el archivo de configuración y los directorios.");
 			}
 
 		}catch(IOException e){ //Capturamos las exceptiones 
 			e.printStackTrace();
-		}
-
-
-		try{
-			//Instanciación del Scanner
-			Scanner teclado = new Scanner(System.in);
-			System.out.println("Dime el nombre del directorio a crear."); //Preguntar nombre del directorio
-			String nombreDirectorio = teclado.nextLine(); //Recibir nombre del directorio
-			Path rutaCompleta = path.resolve(nombreDirectorio); //Añadimos al path que es la direccion del directorio del proyecto, el nombre del directorio
-			
-			
-			if(Files.exists(rutaCompleta)){
-				System.out.println("El directorio ya existe.");
-			}
-			else{
-				System.out.println("El directorio no existe y se va a crear en la ruta " + rutaCompleta);
-				Files.createDirectories(rutaCompleta);
-			}
-		}
-		catch(IOException e){
-			System.out.println("Error al crear el directorio");
-		}
-		
+		}	
 	
 	}
 }
